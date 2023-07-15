b66b0788b2645fc6acafc15607c9a9375cf449e3
Sanju1301-hub <sankusanjana@gmail.com>
2023-07-15 14:40:14 +0000

    Added

--- election_sanjana.sql
+++ election_sanjana.sql
@@ -1,16 +1,22 @@
 select * from election;
 
+--Creating Index
+create index dataget on election (st_name,partyabbre,partyname,year);
+
+
 -- 1
 select CAND_SEX, Year ,count(CAND_SEX)
 from election
 group by cand_sex,year
 having cand_sex='F';
 
+
 -- 2
 select st_name, YEAR, count(st_name) as total_candidates
 from election
 group by st_name, YEAR
 order by st_name asc;
+
 
 -- 3
 select  st_name , sum(totvotpoll) 
@@ -29,8 +35,8 @@ group by st_name;
 
 -- 5
 
-select partyname,sum(totvotpoll) AS TotalVotes
+select partyname,sum(TOTVOTPOLL) as TotalVotes
 from election
-where st_name='Uttar Pradesh'
-group by partyname order by TotalVotes DESC
-limit 5; 
+where st_name='Uttar Pradesh' and year='2014'
+group by partyname
+fetch first 5 rows only;
