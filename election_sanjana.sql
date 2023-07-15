6de3b03bc9fb700c270a720fc28322678bafac29
elintrovertido <angadipetasanthu@gmail.com>
2023-07-15 13:52:23 +0000

    commit.

--- election_sanjana.sql
+++ election_sanjana.sql
@@ -30,17 +30,7 @@ where year='2004'
 group by st_name;
 
 
-<<<<<<< HEAD:election_santhosh.sql
 select partyname, sum(totvotpoll) as TotalVotes from election
 where st_name = 'Uttar Pradesh' and year = '2014' and rownum < 6
 group by partyname
 order by sum(totvotpoll) desc;
-=======
--- 5
-
-select partyname,sum(totvotpoll) AS TotalVotes
-from election
-where st_name='Uttar Pradesh'
-group by partyname order by TotalVotes DESC
-limit 5; 
->>>>>>> 6b38c863fcba6393ac2832fa21647694b5e6dac1:election_sanjana.sql
