819b0ccd1094e1bb7ea31b750eb70c513d985d64
elintrovertido <angadipetasanthu@gmail.com>
2023-07-15 13:45:19 +0000

    Question Five Added.

--- election_santhosh.sql
+++ election_santhosh.sql
@@ -1,12 +1,13 @@
 select * from election;
 
-create index dataget on election (st_name);
+create index dataget on election (st_name, PARTYABBRE, PARTYNAME, year);
 
 -- 1
-select CAND_SEX, Year ,count(CAND_SEX)
+select CAND_SEX, Year ,count(CAND_SEX) as totalFemaleCandidates
 from election
 group by cand_sex,year
-having cand_sex='F';
+having cand_sex='F'
+order by year;
 
 -- 2
 select st_name, YEAR, count(st_name) as total_candidates
@@ -15,7 +16,7 @@ group by st_name, YEAR
 order by st_name asc;
 
 -- 3
-select  st_name , sum(totvotpoll) 
+select  st_name , sum(totvotpoll) as TotalVotes
 from election
 where partyabbre='BJP' and year='1987'
 group by st_name;
@@ -30,15 +31,7 @@ group by st_name;
 
 -- 5
 
-select partyname, sum(totvotpoll) from election
-where st_name = 'Andhra Pradesh' and year = '2014' and rownum < 6
+select partyname, sum(totvotpoll) as TotalVotes from election
+where st_name = 'Uttar Pradesh' and year = '2014' and rownum < 6
 group by partyname
-order by sum(totvotpoll) desc
-;
-
-
-
-
-select st_name, count(st_name) from election
-where year = '2014'
-group by st_name;
+order by sum(totvotpoll) desc;
