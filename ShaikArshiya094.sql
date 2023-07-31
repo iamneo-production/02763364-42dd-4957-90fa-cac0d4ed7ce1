65b1e40b686a5bd6d24ad6054c836a24496ce033
ShaikArshiya094 <20pa5a0517@vishnu.edu.in>
2023-07-31 10:07:43 +0000

    c

--- ShaikArshiya094.sql
+++ ShaikArshiya094.sql
@@ -1,6 +1,7 @@
 --Index created for table election of online voting 
 
-create index idex on election (st_name, PARTYABBRE, PARTYNAME, cand_sex,year);
+create index i on election (st_name, PARTYABBRE, PARTYNAME, cand_sex,year);
+
 
 -- 1 Total count of female candidates participating in each year
 
