b3c003acfbbcd0ea268bffb24e8aaed9b9bd8352
Shamu1080 <shaheminagulnar828@gmail.com>
2023-07-31 14:48:59 +0000

    c

--- Shamu1080.sql
+++ Shamu1080.sql
@@ -8,7 +8,7 @@ select CAND_SEX, Year ,count(CAND_SEX) as totalFemaleCandidates
 from election
 group by cand_sex,year
 having cand_sex='F'
-order by year ASC;
+order by year asc;
 
 -- 2 Total candidates participated in election at each state in each year
 select st_name, YEAR, count(st_name) as total_candidates
